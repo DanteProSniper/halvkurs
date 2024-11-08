6f4592f36feca7aa6bca5862b83ba7bac032e326
Dante Åkring <danakr0609@edu.halmstad.se>
2024-11-08 13:17:26 +0100

    first lesson

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const fs = require("fs");
+app.listen(port, _=>console.log(`http://localhost:${port}`));
+
+
+
+app.get("/", home);
+
+
+function home(req, res){
+
+    res.send("Hello world!!!");
+
+}
